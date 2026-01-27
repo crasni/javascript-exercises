11cec8a1e6635624aba9a307e436c462e7fde1bc
crasni <lucaslin99@gmail.com>
2026-01-27 22:53:55 +0800

    solution to foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) 
+        return "ERROR";
+    return (a + b) * (Math.abs(b - a) + 1) / 2;
 };
 
 // Do not edit below this line
