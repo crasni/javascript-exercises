b89312bb94ce5b7b3cb491b01c8f63d654bc16d1
crasni <lucaslin99@gmail.com>
2026-01-27 22:48:31 +0800

    solution to foundations/07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let res_str = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        res_str.push(str[i]);
+    }
+    return res_str.join("");
 };
 
 // Do not edit below this line
