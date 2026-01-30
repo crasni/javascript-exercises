4eb494dd7a1d55e1f6e9c82baad4125692060e1c
crasni <lucaslin99@gmail.com>
2026-01-30 21:27:33 +0800

    solution to foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const len = string.length
+    l = 0; r = len - 1; 
+    while (l < r) {
+        if (!/^[A-Za-z0-9]$/.test(string[l])) {
+            l++;
+            continue;
+        } 
+        if (!/^[A-Za-z0-9]$/.test(string[r])) {
+            r--;
+            continue;
+        }
+        if (string[l].toLowerCase() != string[r].toLowerCase()) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
