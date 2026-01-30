5d49f6f59935023181669f50343a2ca696b36814
crasni <lucaslin99@gmail.com>
2026-01-30 21:21:00 +0800

    solution to foundations/12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((tt, num) => tt + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, num) => prod * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, ex) {
+	let res = 1;
+  for (let i = 0; i < ex; i++) {
+    res *= base
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1;
+  for (let k = a; k >= 2; k--) {
+    res *= k;
+  }
+  return res;
 };
 
 // Do not edit below this line
