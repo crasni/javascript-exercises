159dc237338820d6e0e3ff010f8b6de858735b8d
crasni <lucaslin99@gmail.com>
2026-01-30 21:35:10 +0800

    solution to foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const lol = Number(input)
+    if (lol < 0) return "OOPS";
 
+    let prev = 0, cur = 1;
+    for (let i = 0; i < lol; i++) {
+        [prev, cur] = [cur, prev + cur];
+    }
+    return prev;
 };
 
 // Do not edit below this line
