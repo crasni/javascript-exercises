d3b13038d432c628a65ba60fbd37b04b9cd72601
crasni <lucaslin99@gmail.com>
2026-01-30 21:48:18 +0800

    solution to foundations/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppl) {
+    return ppl.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const personDeath = person.yearOfDeath ?? currentYear;
+        const oldestDeath = oldest.yearOfDeath ?? currentYear;
+        return oldestDeath - oldest.yearOfBirth > personDeath - person.yearOfBirth ? oldest : person;
+    }, {name: "baby", yearOfBirth: 0, yearOfDeath: 0});
 };
 
 // Do not edit below this line
